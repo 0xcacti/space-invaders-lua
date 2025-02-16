e09d93c0961b1d038a245b54ee2f1c1ca87fb205
0xcacti <0xcacti@gmail.com>
2025-02-16 00:28:19 -0500

    saving up my repo, need to do other work

--- main.lua
+++ main.lua
@@ -1,19 +1,17 @@
 function love.load()
-    -- Initialization code, runs once at the start
-    love.graphics.setBackgroundColor(0.2, 0.2, 0.2) -- Set background to dark gray
-    message = "Hello, World!"
-    font = love.graphics.newFont(32)                -- Load a font at size 32
-    love.graphics.setFont(font)
+    fruits = { "apple", "banana" }
+    table.insert(fruits, "orange")
 end
 
 function love.update(dt)
-    -- Called every frame to update game state
-    -- `dt` is the time delta since the last frame
+    if love.keyboard.isDown("right") then
+        x = x + 100 * dt
+    elseif love.keyboard.isDown("left") then
+        x = x - 100 * dt
+    end
 end
 
 function love.draw()
-    -- Called every frame to draw on the screen
-    love.graphics.setColor(1, 1, 1) -- Set color to white
-    love.graphics.printf(message, 0, love.graphics.getHeight() / 2 - font:getHeight() / 2, love.graphics.getWidth(),
-        "center")
+    -- love.graphics.rectangle("fill", x, 200, 50, 80)
+    love.graphics.print("Test", 100, 100)
 end
