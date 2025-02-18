12d2dfe3a19404cc7e2f2a1f45cd03ada030d735
0xcacti <0xcacti@gmail.com>
2025-02-18 11:13:46 -0500

    forgot to commit last night

--- main.lua
+++ main.lua
@@ -1,27 +1,23 @@
-require("example")
-print(test)
 function love.load()
-    listOfRectangles = {}
-end
+    Object = require("classic")
+    require("shape")
+    require("circle")
+    require("rectangle")
 
-function createRect()
-    table.insert(listOfRectangles, { x = 0, y = 0, width = 100, height = 100, speed = 100 })
+    r1 = Rectangle(100, 100, 200, 50)
+    r2 = Circle(350, 80, 40)
+
 end
 
 function love.keypressed(key)
-    if key == "space" then
-        createRect()
-    end
 end
 
 function love.update(dt)
-    for i, rect in ipairs(listOfRectangles) do
-        rect.x = rect.x + rect.speed * dt
-    end
+    r1:update(dt)
+    r2:update(dt)
 end
 
 function love.draw()
-    for i, rect in ipairs(listOfRectangles) do
-        love.graphics.rectangle("fill", rect.x, rect.y, rect.width, rect.height)
-    end
+    r1:draw()
+    r2:draw()
 end
