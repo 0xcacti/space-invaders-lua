9ff8fcf71a12eeaaf8050016a855759d4ae55c7a
0xcacti <0xcacti@gmail.com>
2025-02-19 01:42:33 -0500

    save

--- main.lua
+++ main.lua
@@ -1,32 +1,21 @@
 function love.load()
-    circle = {}
-    circle.x = 100
-    circle.y = 100
-    circle.radius = 25
-    circle.speed = 200
+    frames = {}
+    table.insert(frames, love.graphics.newImage("assets/jump1.png"))
+    table.insert(frames, love.graphics.newImage("assets/jump2.png"))
+    table.insert(frames, love.graphics.newImage("assets/jump3.png"))
+    table.insert(frames, love.graphics.newImage("assets/jump4.png"))
+    table.insert(frames, love.graphics.newImage("assets/jump5.png"))
+
+    currentFrame = 1
 end
 
 function love.update(dt)
-    mouse_x, mouse_y = love.mouse.getPosition()
-    angle = math.atan2(mouse_y - circle.y, mouse_x - circle.x)
-    cos = math.cos(angle)
-    sin = math.sin(angle)
-
-    --Make the circle move towards the mouse
-    circle.x = circle.x + circle.speed * cos * dt
-    circle.y = circle.y + circle.speed * sin * dt
+    currentFrame = currentFrame + 10 * dt
+    if currentFrame >= 6 then
+        currentFrame = 1
+    end
 end
 
 function love.draw()
-    love.graphics.circle("line", circle.x, circle.y, circle.radius)
-
-    --Print the angle
-    love.graphics.print("angle: " .. angle, 10, 10)
-
-    --Here are some lines to visualize the velocities
-    love.graphics.line(circle.x, circle.y, mouse_x, circle.y)
-    love.graphics.line(circle.x, circle.y, circle.x, mouse_y)
-
-    --The angle
-    love.graphics.line(circle.x, circle.y, mouse_x, mouse_y)
+    love.graphics.draw(frames[math.floor(currentFrame)])
 end
