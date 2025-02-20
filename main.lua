9554066c25ddf65e3447cff2276e72a1a341121a
0xcacti <0xcacti@gmail.com>
2025-02-20 17:21:21 -0500

    save

--- main.lua
+++ main.lua
@@ -1,21 +1,34 @@
-function love.load()
-    song = love.audio.newSource("assets/song.ogg", "stream")
-    song:setLooping(true)
-    song:play()
+io.stdout:setvbuf("no")
 
-    sfx = love.audio.newSource("assets/sfx.ogg", "static")
+function love.load()
+    circle = { x = 100, y = 100 }
+    bullets = {}
 end
 
 function love.update(dt)
-
-end
-
-function love.keypressed(key)
-    if key == "space" then
-        sfx:play()
+    for i, v in ipairs(bullets) do
+        v.x = v.x + 400 * dt
+        print(v.x)
     end
 end
 
 function love.draw()
+    love.graphics.circle("fill", circle.x, circle.y, 50)
 
+    for i, v in ipairs(bullets) do
+        love.graphics.circle("fill", v.x, v.y, 10)
+    end
+end
+
+function love.keypressed()
+    print("What is the key?", key)
+
+    if key == "space" then
+        shoot()
+    end
+end
+
+function shoot()
+    table.insert(bullets, { x = circle.x, y = circle.y })
+    print("How many bullets?", #bullets)
 end
