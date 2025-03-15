6b52f4af69d08ce046e4500d89ed66b5d3e14f96
0xcacti <0xcacti@gmail.com>
2025-03-15 02:03:14 -0400

    fonts working

--- src/ui/score_board.lua
+++ src/ui/score_board.lua
@@ -7,14 +7,20 @@ function ScoreBoard:new(x, y, score)
     self.max_score = 999999
     self.x = x
     self.y = y
-    local font = love.graphics.newFont("assets/font/pixel-invaders.ttf", 64)
-    self.font = font
-    self.width = font:getWidth("Score: 999999")
-    self.height = font:getHeight()
+    local text_font = love.graphics.newFont("assets/font/text.ttf", 32)
+    local number_font = love.graphics.newFont("assets/font/numbers.ttf", 64)
+    self.text_font = text_font
+    self.number_font = number_font
+    self.width = text_font:getWidth("Score: ") + number_font:getWidth("999999")
+    self.text_height = text_font:getHeight("Score: ")
 end
 
 function ScoreBoard:draw()
-    love.graphics.print("Score: " .. self.score, self.font, self.x, self.y)
+    love.graphics.setFont(self.text_font)
+    love.graphics.print("Score: ", self.x, self.y)
+    love.graphics.setFont(self.number_font)
+
+    love.graphics.print(string.format("%06d", self.score), self.x + self.text_font:getWidth("Score: "), self.y - self.text_height / 2)
 end
 
 return ScoreBoard
